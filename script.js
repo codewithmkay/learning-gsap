dd6ffded9c8ba21e24a3f973515d7dfb58e4ab1b
codewithmkay <aaalembic@gmail.com>
2026-09-11 15:54:43 +0300

    added a gsap.to in the js

---
+++ script.js
@@ -0,0 +1,14 @@
+//in the  below code, gsap.to is used to animate the element with the class "box" by 
+// moving it 300 pixels to the right over a duration of 2 seconds using the GSAP 
+
+
+
+gsap.to(".box", {
+    x:300,
+    duration: 2   
+})
+
+// gsap.to("ELEMENT", {
+//     PROPERTY: VALUE,
+//     duration: TIME
+// });
